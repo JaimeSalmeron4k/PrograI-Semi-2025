65819fad66c7df96dddcaea672ea7cd2eb1a68bb
Jaime Salmeron <expresscliente138@gmail.com>
2025-11-04 22:38:01 -0600

    Terminacion del CRUD (Crear, Actualizar, Eliminar y Consultar) de alumnos.

--- webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0ad99733ae2080d55f60a78250f57a5727e79a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacb44776a332d245a9121364d396e4ffb60083a")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
